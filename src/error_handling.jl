cdcc97a8795e4b2b332ab25e261e529145801caa
Tom Kwong <tk3369@gmail.com>
2023-08-12 23:31:02 -0700

    Print stacktrace on error

--- src/error_handling.jl
+++ src/error_handling.jl
@@ -1,9 +1,10 @@
 function register_error_handler(bot)
-    register_error_handler!(bot) do client, message, ex, args...
+    register_error_handler!(bot) do client, message, ex, stack_frames, args...
         if ex isa IgUserError
             reply(client, message, ex.message)
         else
             @error "Non user exception" ex
+            foreach(x -> println(x[1], ": ", x[2]), enumerate(stack_frames))
         end
     end
 end
